a65da12bec36ef22fba5d555b7caf1193146e654
Mikhail <mmskazak@gmail.com>
2025-02-16 22:30:48 +0300

    upd checker dummy respomse and request

--- is_request/dummy.go
+++ is_request/dummy.go
@@ -1,10 +1,5 @@
 package isrequest
 
-// Request for all messages
-func IsDummy(message []byte) bool {
-	return true
-}
-
 // Dummy is a default request checker that always returns false
 func Dummy(message []byte) bool {
 	return false
--- is_response/dummy.go
+++ is_response/dummy.go
@@ -1,10 +1,5 @@
 package isresponse
 
-//Response for all messages
-func IsDummy(message []byte) bool {
-	return true
-}
-
 // Dummy is a default response checker that always returns false
 func Dummy(message []byte) bool {
 	return false
